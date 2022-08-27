90459e841f7bfa846355e34002b7610de00d7abd
jaydxn1 <pangjunrong99@gmail.com>
2022-08-27 17:40:38 +0800

    added run code for localhost 5000

--- hello.py
+++ hello.py
@@ -4,5 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
-    
+    return "<p>Hello, Test!</p>"
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=5000)
